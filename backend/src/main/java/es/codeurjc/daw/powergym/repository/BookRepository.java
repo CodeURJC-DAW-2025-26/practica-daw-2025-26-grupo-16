5b1206fea5a0330a1dc8a6b950443232948a3d5c
aadri-2003 <117304862+aadri-2003@users.noreply.github.com>
2026-03-01 23:24:06 +0100

    Delete unused files

--- backend/src/main/java/es/codeurjc/daw/powergym/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package es.codeurjc.daw.powergym.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.codeurjc.daw.powergym.model.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-}
--- backend/src/main/java/es/codeurjc/daw/powergym/repository/ShopRepository.java
+++
@@ -1,9 +0,0 @@
-package es.codeurjc.daw.powergym.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.codeurjc.daw.powergym.model.Shop;
-
-public interface ShopRepository extends JpaRepository<Shop, Long> {
-
-}
