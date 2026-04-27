3529a15869d017fd2a78879e545904045a10c24f
aadri-2003 <117304862+aadri-2003@users.noreply.github.com>
2026-04-27 14:49:58 +0200

    Fix error with URL

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
 			"/api": {
-				target: "https://localhost:8443",
+				target: "https://localhost:8443/api",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
         secure: false
